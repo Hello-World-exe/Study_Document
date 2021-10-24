f2472326443eb8f383cfc4da1b3de0ff52cbc94c
Curry5004 <91974726+Curry5004@users.noreply.github.com>
2021-10-24 17:42:06 +0900

    kosmo

---
+++ 0004_홍지환/java/kosmo/studyChap01/src/chap808/Hi.java
@@ -0,0 +1,15 @@
+package chap808;
+
+public class Hi {
+public static void main(String[] args) {
+	int intValue = 10;
+	double doubleValue=5.5;
+	int result = intValue + doubleValue;
+	
+	intValue=10.0;
+	
+	
+	
+	
+}
+}
